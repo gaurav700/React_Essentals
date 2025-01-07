e9fb6a874b255162b0bb11946165bee15511ed2b
Gaurav Jangid <60379544+gaurav700@users.noreply.github.com>
2025-01-07 18:57:07 +0000

    Using & Outputting Dynamic Values

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@
 // must be stored in the public folder (as it's the case by default in this project)
 
 function App() {
+  const reactDescriptions = ["Fundamental", "Cruicial", "Core"];
+  function getRandom(max) {
+    return Math.floor(Math.random() * (max + 1));
+  }
   return (
     <div>
       <header>
         <img src="assets/react-core-concepts.png" alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
+          {reactDescriptions[getRandom(2)]} React concepts you will need for
+          almost any app you are going to build!
         </p>
       </header>
 
